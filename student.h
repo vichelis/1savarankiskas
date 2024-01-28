d451d2cb3a47cce114d4b93f918853452f3d19fc
Arnas <86932467+vichelis@users.noreply.github.com>
2024-01-28 17:20:24 +0200

    Update student.h

--- student.h
+++ student.h
@@ -1,1 +1,38 @@
+#ifndef STUDENT_H
+#define STUDENT_H
 
+#include <iostream>
+#include <vector>
+
+class Student {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::vector<double> homeworkResults;
+    double examResult;
+    double finalScore;
+
+public:
+    Student();
+    explicit Student(int numHomeworks);
+
+    ~Student();
+
+    Student(const Student& other);
+
+    Student& operator=(const Student& other);
+
+    friend std::istream& operator>>(std::istream& is, Student& student);
+    friend std::ostream& operator<<(std::ostream& os, const Student& student);
+
+    double calculateFinalScoreAverage() const;
+    double calculateFinalScoreMedian() const;
+
+    std::string getFirstName() const;
+    std::string getLastName() const;
+
+    bool operator<(const Student& other) const;
+    bool operator==(const Student& other) const;
+};
+
+#endif // STUDENT_H
